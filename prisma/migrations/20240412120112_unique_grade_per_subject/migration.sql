001447e6332fc2d77fe4814961d8bca87a2324ba
Victor Gabriel <victorgabmariano@gmail.com>
2024-04-12 13:06:43 -0300

    Revert "BREAKING CHANGE: make grade unique for subject"
    This reverts commit c86ce8174cbf0405f6fd8657dc28d4f099260231.

--- prisma/migrations/20240412120112_unique_grade_per_subject/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[studentId,subjectId]` on the table `Grade` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Grade_studentId_subjectId_key` ON `Grade`(`studentId`, `subjectId`);
